cc172732741cf860c308ceefb4535a6f3e07f7a9
Eva262 <mystudy6329@gmail.com>
2021-07-20 19:04:28 +1000

    ДЗ 1, корректировки наименований переменных и локации кода

--- les1/Variable.java
+++ les1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
     public static void main(String[] args) {
-        byte yadra = 2;
+        byte core = 2;
         short ram = 8;
         int logfile = 123456789 ;
         long hdd = 1005421614656l;
         float cpu = 3.20f;
         char os = 'W';
-        double sborkaos = 19042.1083d;
-        boolean t = true;
-            System.out.println("Ядра= " + yadra);
-            System.out.println("ОЗУ= " + ram);
-            System.out.println("Лог-файл= " + logfile);
-            System.out.println("Жесткий диск= " + hdd);
-            System.out.println("Процессор= " + cpu);
-            System.out.println("Операционная система= " + os + "indows");
-            System.out.println("Версия сборки ОС= " + sborkaos);
-            System.out.println("The " + t + " is out there.@X-files");
+        double version = 19042.1083d;
+        boolean word = true;
+        System.out.println("Ядра = " + core);
+        System.out.println("ОЗУ = " + ram);
+        System.out.println("Лог-файл = " + logfile);
+        System.out.println("Жесткий диск = " + hdd);
+        System.out.println("Процессор = " + cpu);
+        System.out.println("Операционная система = " + os + "indows");
+        System.out.println("Версия сборки ОС = " + version);
+        System.out.println("The " + word + " is out there.@X-files");
     }
 
 }
