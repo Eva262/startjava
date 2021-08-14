a1649eff961c0b2353c0992fb5f748b9515dddf6
Eva262 <mystudy6329@gmail.com>
2021-08-14 11:27:55 +1000

    форматирование кода, замена присвоения на сложение с присвоением, изменение имени переменной

--- les1/Cycle.java
+++ les1/Cycle.java
@@ -4,22 +4,23 @@ public class Cycle {
         for (int i = 0; i <= 20; i++) {
             System.out.print(i + " ");
         }
+        
         System.out.println("\nЧисла от -6 до 6 с шагом итерации 2, цикл While");
         int i = -6; 
-        while (i <= 6){
+        while (i <= 6) {
             System.out.println(i);
-            i = i + 2;
+            i += 2;
         }
+        
         //Сумма нечетных чисел, цикл Do While
         int j = 10;
-        int result = 0;
+        int sumOdd = 0;
         do {
-            if ( j % 2 > 0){
-                result = result + j;
+            if (j % 2 > 0) {
+                sumOdd += j;
             }
             j++;
         } while (j < 20);
-        System.out.println("Сумма нечетных чисел от 10 до 20 равна " + result);
+        System.out.println("Сумма нечетных чисел от 10 до 20 равна " + sumOdd);
     } 
-
 }        
